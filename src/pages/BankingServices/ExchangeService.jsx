0f1212e27767d14b26d27216f91c3d6031bd7576
1192811 <abdulkalamp588@gmail.com>
2023-09-14 15:34:36 +0530

    feat:{abdul} added banking services

---
+++ src/pages/BankingServices/ExchangeService.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function ExchangeService() {
+    return (
+        <div>
+            <section id="exchange" class="mt-5">
+                <h2>Exchange Services</h2>
+                <p>
+                    Our exchange services provide you with convenient currency exchange solutions.
+                    Whether you're traveling abroad for business or leisure, we offer competitive
+                    exchange rates for various currencies.
+                </p>
+                <p>
+                    Visit one of our branches or use our online exchange platform to buy or sell
+                    foreign currencies. We also provide travel cards for a secure and hassle-free
+                    international travel experience.
+                </p>
+            </section>
+        </div>
+    )
+}
---
+++ src/pages/BankingServices/InsuranceService.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function InsuranceService() {
+    return (
+        <div>
+
+            <section id="insurance" class="mt-5">
+                <h2>Insurance</h2>
+                <p>
+                    Protect yourself and your loved ones with our comprehensive insurance coverage.
+                    From life insurance to health insurance and property insurance, we've got you protected.
+                </p>
+                <p>
+                    Our experienced insurance agents will guide you through the process of selecting
+                    the right insurance plans to safeguard your financial future.
+                </p>
+            </section>
+        </div>
+    )
+}
